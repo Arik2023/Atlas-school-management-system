deb137793a7fc4659e3331a17f8c8ab6578a2eca
Cagatay Arik <cagatay.arik@gmail.com>
2024-01-17 14:02:50 +0000

    Version1

---
+++ src/test/java/com/techproeducation/backendproject/initialwork/Atlasschoolmanagementbackend/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techproeducation.backendproject.initialwork.Atlasschoolmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
